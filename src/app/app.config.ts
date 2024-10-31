30b0507e3963099d0124762bfe69a2acaa7ba7dd
Fernando Vazquez <fervaztir15@iesfidiana.es>
2024-10-31 13:47:26 +0100

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"recetario-fda97","appId":"1:674284180019:web:d1dc68cb61bc879b808946","storageBucket":"recetario-fda97.appspot.com","apiKey":"AIzaSyAB21C_rA7ntbgLsB_VGJQqhxdc5kHmjd4","authDomain":"recetario-fda97.firebaseapp.com","messagingSenderId":"674284180019","measurementId":"G-0RJ0RVGX1N"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production:false,
+    
+    api:{
+        nationalit:'www.themealdb.com/api/json/v1/1/list.php?a=list',
+        categories:'www.themealdb.com/api/json/v1/1/list.php?c=list',
+        listByCategory:'www.themealdb.com/api/json/v1/1/filter.php?c=',
+        listByNationality:'www.themealdb.com/api/json/v1/1/filter.php?a=',
+        viewRecipe:'www.themealdb.com/api/json/v1/1/lookup.php?i='
+    }
+};
